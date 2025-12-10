036dc5ced57273c935ccfc02be0aa65fed2af72e
judemcastillo <judemcastillo@gmail.com>
2025-12-10 14:35:21 +0800

    Fullstack setup frontend-React backend-Node,express,Prisma,postgreswl

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="border-2 border-amber-500 text-xl p-10 rounded">Hello</div>
+		</>
+	);
+}
+
+export default App;
---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+	console.log(`Listening on port:${PORT}`);
+});
